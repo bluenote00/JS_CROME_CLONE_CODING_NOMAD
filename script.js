89b0727f848b6e092bbee5fd8782b77051dab6f9
annej <god0937@gmail.com>
2023-04-11 13:42:48 +0900

    const and let

---
+++ script.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 2;
+const veryLong = "anne";
+
+console.log(a + 2);
+console.log(a * 2);
+console.log(a / 2);
+console.log("hello " + veryLong);
+
+veryLong = "ara";
+
+console.log("hello " + veryLong);
+
+// const : 상수, 값이 바뀔 수 없다
+// let : 값이 바뀔 수 있다. (업데이트 가능성이 있는 변수에 사용)
+// var : 어떤 규칙도 없음 -> 어떻게 사용할 것인지 예측하기 힘들다. (왠만하면 사용하지 않는게 좋음)
+
