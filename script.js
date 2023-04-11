939571244b91cbab6a6954fb90e1f01925a234d0
annej <god0937@gmail.com>
2023-04-11 16:37:32 +0900

    arrays

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ console.log(something, amIFat);
 // 여기서 something은 정의되지 않았기에 undefined로 뜨며,amIFat은 null값이 뜬다.
 
 // null : 값이 아무것도 없다 (false와 다르다. false에는 아니라는 값이 있음), null은 자연적으로 생겨나지 않는다. 값이 없음을 표현할떄 사용 '비어있다'
+
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+
+//const daysOfWeek = mon + tue + wed;
+
+const daysOfWeek = [mon, tue, wed];
+
+
+// 0번째 element 받아오기
+console.log(daysOfWeek);
+
+//array에 추가하기 - 가장 마지막 배열에 추가 된다.
+daysOfWeek.push("sun");
+
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
+
+console.log(toBuy);
