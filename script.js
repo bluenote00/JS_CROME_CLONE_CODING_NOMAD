ef8cb3120dbe6cace322bf23f1c306d01e96c13d
annej <god0937@gmail.com>
2023-04-19 15:18:47 +0900

    Saving Username

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+//const link = document.querySelector("a");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 // value값 submit 이벤트
 function onLoginSubmit(event) {
@@ -9,9 +11,22 @@ function onLoginSubmit(event) {
     // form을 submit하면 브라우저는 기본적으로 페이지를 새로고침한다.
     // preventDefault는 사전에 이벤트가 발생하지 않도록 막는다. 
     // preventDefault 함수를 추가해 브라우저가 기본 동작을 실행하지 못하게 막는다.
-    console.log(loginInput.value);
+    // console.log(loginInput.value);
+
+    loginForm.classList.add("HIDDEN_CLASSNAME");
+    const username = loginInput.value;
+
+    localStorage.setItem("username", username);
+    // 브라우저 상의 API, LocalStorage에 데이터를 저장한다!
+    // application - localStorage에서 확인 가능, 그러나 새로고침을 하면 사라진다.
+
+    //greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME); 
 }
 
+
+/*
 function handleLinkClick(event) {
     event.preventDefault();
     // preventDefault가 링크 이동 이벤트를 막고 있다!
@@ -19,10 +34,10 @@ function handleLinkClick(event) {
     // alert("click!")
     // alert는 창이 닫히기전까지 모든 이벤트를 막는다.
 }
-
+*/
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+// link.addEventListener("click", handleLinkClick);
 
 
 /*
