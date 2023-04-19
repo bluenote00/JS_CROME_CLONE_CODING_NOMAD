fa851ea23ee23d18c4ec2c24de8cdd92c7219c08
annej <god0937@gmail.com>
2023-04-19 12:30:40 +0900

    LOGIN - Input value (preventDefault)

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-    //console.dir(loginInput.value); //input창의 value 값을 확인 (입력된 값)
-    //console.log("click");
-    const username = loginInput.value;
-    if(username === "") {
-        alert("Please write your name.")
-    } else if (username.length > 15) {
-        alert("It's too long.")        
-    }
+
+// value값 submit 이벤트
+function onLoginSubmit(event) {
+    event.preventDefault(); 
+    // form을 submit하면 브라우저는 기본적으로 페이지를 새로고침한다.
+    // preventDefault는 사전에 이벤트가 발생하지 않도록 막는다. (브라우저가 기본 동작을 실행하지 못하게 막는다.)
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+
+
+loginForm.addEventListener("submit", onLoginSubmit);
