8c013735085189158340ed73e9b62ba1e8c4e192
annej <god0937@gmail.com>
2023-04-19 23:51:08 +0900

    Loading Username

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 //const link = document.querySelector("a");
 
+
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 // value값 submit 이벤트
 function onLoginSubmit(event) {
@@ -13,16 +15,14 @@ function onLoginSubmit(event) {
     // preventDefault 함수를 추가해 브라우저가 기본 동작을 실행하지 못하게 막는다.
     // console.log(loginInput.value);
 
-    loginForm.classList.add("HIDDEN_CLASSNAME");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
     // 브라우저 상의 API, LocalStorage에 데이터를 저장한다!
     // application - localStorage에서 확인 가능, 그러나 새로고침을 하면 사라진다.
 
     //greeting.innerText = "Hello " + username;
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME); 
+    paintGreetings(username);
 }
 
 
@@ -47,3 +47,20 @@ addEventListener의 함수에서 object에 대한 자리만 할당해주면
 해당 이벤트가 발생시킨 정보들에 대한 object들을 볼 수 있다!
 이때 해당 이벤트가 가진 기본 Default값을 발생시키지 않기 하게 위해선 preventDefault를 이용하여 막을 수 있다!
 */
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greetings
+    paintGreetings(username);
+}
