b083c3a44f2d73ba7ae6dca7604167541d9f03e5
annej <god0937@gmail.com>
2023-04-19 12:01:30 +0900

    LOGIN - Input value
    JS로 글자 수 제한하기

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-    console.dir(loginInput.value); //input창의 value 값을 확인 (입력된 값)
-    console.log("click");
+    //console.dir(loginInput.value); //input창의 value 값을 확인 (입력된 값)
+    //console.log("click");
+    const username = loginInput.value;
+    if(username === "") {
+        alert("Please write your name.")
+    } else if (username.length > 15) {
+        alert("It's too long.")        
+    }
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
